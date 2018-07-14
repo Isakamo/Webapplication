89e5755c421291e29d813ab45ca8fa9b76aaaf22
Isakamo <sakamoto.072911@gmail.com>
2018-07-14 13:30:00 +0900

    controller追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,26 @@
 class TopController < ApplicationController
+  def index
+    #@tilte = 'THE 雑談'
+    @boards = Board.all
+    @newBoards = Board.new
+    #render template: 'top/index'
+  end
+
+  def show
+    @boards = Board.find(params[:id])
+    @thres = Thre.where(board_id: params[:board_id])
+  end
+
+  def create
+    #@boards = Board.new(params[:name].permit(:name))
+    @boards = Board.new
+    @boards.save
+    redirect_to top_path
+  end
+
+  def delete
+    @boards = Board.find(params[:id])
+    @boards.destroy
+    redirect_to top_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'top', to: 'top#index'
+  get 'top/show/:id' => 'top#show', as: :top_show
+  post 'top/create' => 'top#create'
+  delete 'top/delete/:id' => 'top#delete', as: :top_delete
+  #get 'top/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
