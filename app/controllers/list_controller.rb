150ebdfbc892010db21a06af0e8e0bf3b10d1428
Isakamo <sakamoto.072911@gmail.com>
2018-07-20 20:16:39 +0900

    second issu

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,7 +1,14 @@
 class ListController < ApplicationController
   def create
-    @thre = (params[:thre].permit(:board_id, :name, :user))
-    @thre.save
+    @thres = Thre.new(params[:thre].permit(:board_id, :name, :user))
+    @thres.save
     redirect_to top_show_path(params[:thre]['board_id'])
+  end
+
+  def show
+    @boards = Board.find(params[:id])
+    @thres = Thre.where(board_id: params[:id])
+    @responses = Response.where(thre_id: params[:id])
+    @newresponses = Response.new(:thre_id => params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'top', to: 'top#index'
   post 'top/create', to: 'top#create'
-  get 'top/show/:id', to: 'top#show', as: :top_show
+  get 'top/list/:id', to: 'top#show', as: :top_show
   delete 'top/delete/:id', to: 'top#delete', as: :top_delete
   post 'list/create', to: 'list#create', as: :list_create
+  get 'res/:id', to: 'list#show', as: :list_show
   #get 'top/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
